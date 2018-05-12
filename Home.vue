005e9ccf05045f3b80b6ce20d2f5a19af29fcaaf
AlexPal4inskiy <alexpal4inskiy@gmail.com>
2018-05-12 16:35:19 +0300

    Create Home.vue

---
+++ Home.vue
@@ -0,0 +1,32 @@
+<template>
+  <Page class="page">
+    <ActionBar class="action-bar" title="Home"/>
+
+    <StackLayout>
+      <Button class="btn btn-primary" @tap="login">Facebook login</Button>
+    </StackLayout>
+
+  </Page>
+</template>
+
+<script>
+	import * as Facebook from "nativescript-facebook";
+
+	export default {
+		created() {
+			Facebook.init('APP_ID');
+		},
+		methods: {
+			login() {
+				Facebook.login((err, fbData) => {
+					if (err) {
+						alert("Error during login: " + err.message);
+					} else {
+						console.log(fbData.token);
+					}
+				});
+				
+			}
+		}
+	}
+</script>
